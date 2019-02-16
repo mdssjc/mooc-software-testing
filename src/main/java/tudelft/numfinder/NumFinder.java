60c7b68688be800808a855a636c1a6dfd9055af5
Marcelo dos Santos <mdssjc@gmail.com>
2019-02-16 10:07:09 -0200

    Correção do bug.

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -1,25 +1,28 @@
 package tudelft.numfinder;
 
 public class NumFinder {
+
     private int smallest = Integer.MAX_VALUE;
     private int largest = Integer.MIN_VALUE;
 
     public void find(int[] nums) {
-        for(int n : nums) {
+        smallest = nums[0];
+        largest = smallest;
 
-            if(n < smallest)
+        for (int n : nums) {
+            if (n < smallest) {
                 smallest = n;
-            else if (n > largest)
+            } else if (n > largest)
                 largest = n;
 
         }
     }
 
-    public int getSmallest () {
+    public int getSmallest() {
         return smallest;
     }
 
-    public int getLargest () {
+    public int getLargest() {
         return largest;
     }
 }
