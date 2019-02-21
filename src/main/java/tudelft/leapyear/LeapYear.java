317f5023bfa21020b9d239e0902f2ca8064c16ff
Marcelo dos Santos <mdssjc@gmail.com>
2019-02-20 21:31:29 -0300

    Formatação e simplificação.

--- src/main/java/tudelft/leapyear/LeapYear.java
+++ src/main/java/tudelft/leapyear/LeapYear.java
@@ -3,10 +3,12 @@ package tudelft.leapyear;
 public class LeapYear {
 
     public boolean isLeapYear(int year) {
-        if ( year % 400 == 0)
+        if (year % 400 == 0) {
             return true;
-        if ( year % 100 == 0)
+        }
+        if (year % 100 == 0) {
             return false;
-        return  ( year % 4 == 0 ) ? true : false;
+        }
+        return year % 4 == 0;
     }
 }
