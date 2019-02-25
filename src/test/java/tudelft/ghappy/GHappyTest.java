c8bb708563fa3234efa4ee1f8efdeab1907908ca
Marcelo dos Santos <mdssjc@gmail.com>
2019-02-24 21:07:24 -0300

    Testes para o gHappy.

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,36 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    private GHappy gHappy;
+
+    @BeforeEach
+    void setUp() {
+        gHappy = new GHappy();
+    }
+
+    @Test
+    void test01() {
+        boolean result = gHappy.gHappy("xxggxx");
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void test02() {
+        boolean result = gHappy.gHappy("xxgxx");
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void test03() {
+        boolean result = gHappy.gHappy("xxggyygxx");
+
+        Assertions.assertFalse(result);
+    }
 }
